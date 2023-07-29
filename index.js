8a3c939af37b5028d89f900ab7dc82390aca9e8c
x.ande <ananfro@live.com>
2023-07-29 16:38:44 +1000

    created prompt with input validation on textValue

---
+++ index.js
@@ -0,0 +1,62 @@
+const inquirer = require("inquirer");
+
+function prompt() {
+
+    const promptVal = inquirer
+        .prompt([
+
+            {
+                type: "input",
+                message: "Please type in text up to 3 characters that will be displayed in the SVG...",
+                name: "textValue",
+                // Throw an error if input is above 3 characters.
+                validate: async (input) => {
+
+                    await this;
+
+                    if (input.length <= 3) return true;
+                    else throw new Error("Please make your response up to 3 characters.");
+
+                },
+            },
+
+            {
+                type: "input",
+                message: "Please choose a text color; it can be a generic color or a HEX color...",
+                name: "textColor",
+            },
+
+            {
+                type: "list",
+                message: "Please choose a shape: Circle, Square or Triangle...",
+                choices: [
+
+                    "Circle",
+                    "Square",
+                    "Triangle"
+
+                ],
+                name: "shapeType",
+            },
+
+            {
+                type: "input",
+                message: "Please choose a shape color; it can be a generic color or a HEX color...",
+                name: "shapeColor",
+            }
+
+        ])
+        .catch((err) => console.log(err));
+
+    return promptVal;
+
+}
+
+async function initProcess() {
+
+    const promptVal = await prompt();
+    console.log(promptVal);
+
+}
+
+initProcess();
